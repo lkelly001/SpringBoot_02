c1ce95ee8da6119d52da6ff32c2c1f2c99ee147d
Laseandra Kelly <Seanie@Laseandras-MacBook-Pro.local>
2017-09-11 16:16:39 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpingBoot02Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBoot02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBoot02Application.class, args);
+	}
+}
